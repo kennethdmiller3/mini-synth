125f7f5fc9433af65fffe07f1857f013854c51a2
kennethdmiller3@gmail.com <kennethdmiller3@gmail.com@23c9ecb3-a1c3-0e6f-be60-cf8a5c0acfb7>
2014-01-14 08:31:33 +0000

    * Simplify key volume envelope display

--- DisplayKeyVolumeEnvelope.cpp
+++ DisplayKeyVolumeEnvelope.cpp
@@ -8,6 +8,7 @@ Per-Key Volume Envelope Display
 
 #include "DisplayKeyVolumeEnvelope.h"
 #include "DisplaySpectrumAnalyzer.h"
+#include "Console.h"
 #include "Keys.h"
 
 static COORD const key_pos = { 12, SPECTRUM_HEIGHT };
@@ -25,15 +26,9 @@ static WORD const env_attrib[EnvelopeState::COUNT] =
 void InitKeyVolumeEnvelopeDisplay(HANDLE hOut)
 {
 	// show the note keys
-	for (int k = 0; k < KEYS; ++k)
-	{
-		char buf[] = { char(keys[k]), '\0' };
-		COORD pos = { SHORT(key_pos.X + k), SHORT(key_pos.Y) };
-		WORD attrib = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
-		DWORD written;
-		WriteConsoleOutputAttribute(hOut, &attrib, 1, pos, &written);
-		WriteConsoleOutputCharacter(hOut, buf, 1, pos, &written);
-	}
+	DWORD written;
+	WriteConsoleOutputCharacterW(hOut, LPCWSTR(keys), KEYS, key_pos, &written);
+	FillConsoleOutputAttribute(hOut, env_attrib[EnvelopeState::OFF], KEYS, key_pos, &written);
 }
 
 void UpdateKeyVolumeEnvelopeDisplay(HANDLE hOut, EnvelopeState::State vol_env_display[])
@@ -43,9 +38,8 @@ void UpdateKeyVolumeEnvelopeDisplay(HANDLE hOut, EnvelopeState::State vol_env_di
 		if (vol_env_display[k] != vol_env_state[k].state)
 		{
 			vol_env_display[k] = vol_env_state[k].state;
-			COORD pos = { SHORT(key_pos.X + k), key_pos.Y };
 			DWORD written;
-			WriteConsoleOutputAttribute(hOut, &env_attrib[vol_env_display[k]], 1, pos, &written);
+			FillConsoleOutputAttribute(hOut, env_attrib[vol_env_display[k]], 1, { key_pos.X + SHORT(k), key_pos.Y }, &written);
 		}
 	}
 }
