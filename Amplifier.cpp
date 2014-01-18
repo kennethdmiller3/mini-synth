db2ada345475aeae78d768e13840ed752fcd9223
kennethdmiller3@gmail.com <kennethdmiller3@gmail.com@23c9ecb3-a1c3-0e6f-be60-cf8a5c0acfb7>
2014-01-18 08:09:15 +0000

    * Missed Amplifier in last checkin...

---
+++ Amplifier.cpp
@@ -0,0 +1,19 @@
+/*
+MINI VIRTUAL ANALOG SYNTHESIZER
+Copyright 2014 Kenneth D. Miller III
+
+Amplifier
+*/
+#include "StdAfx.h"
+
+#include "Amplifier.h"
+#include "Voice.h"
+
+// amplifier configuration
+AmplifierConfig amp_config(1.0f, 0.0f);
+
+// amplifier envelope configuration
+EnvelopeConfig amp_env_config(false, 0.0f, 1.0f, 1.0f, 0.1f);
+
+// amplifier envelope state
+EnvelopeState amp_env_state[VOICES];
