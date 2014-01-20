acf8da50577afb0f10c13fe0b5d3190ddc38443a
kennethdmiller3@gmail.com <kennethdmiller3@gmail.com@23c9ecb3-a1c3-0e6f-be60-cf8a5c0acfb7>
2014-01-20 05:23:30 +0000

    * Improve IntegratedPolyBLEP - Closer approximation of cosine shape - Can use a smaller width scale factor

--- PolyBLEP.h
+++ PolyBLEP.h
@@ -4,7 +4,11 @@ MINI VIRTUAL ANALOG SYNTHESIZER
 Copyright 2014 Kenneth D. Miller III
 
 Polynomial Bandlimited Step
+(based on Valimaki/Huovilainen PolyBLEP)
 */
+
+// PolyBLEP
+// Polynomial correction for C0 (value) discontinuity
 
 // width of bandlimited step relative to the sample period
 const float POLYBLEP_WIDTH = 1.5f;
@@ -12,10 +16,14 @@ const float POLYBLEP_WIDTH = 1.5f;
 // approximation of 0.5 * s * (sin((t * M_PI) / (w + w)) - sign(t))
 
 // cubic version:
+// (this doesn't seem to work as well as the quadratic version
 // +: (3t - t^3 - 2) * s * 0.25f
 // -: (3t - t^3 + 2) * s * 0.25f
+//	s *= 0.5f;
+//	float const p = (3 - t * t) * t * 0.5f * s;
+//	return t >= 0 ? p - s : p + s;
 
-// Valimaki/Huovilainen PolyBLEP
+// general case for value step of s units
 inline float PolyBLEP(float t, float w, float s)
 {
 	if (fabsf(t) >= w)
@@ -39,37 +47,44 @@ inline float PolyBLEP(float t, float w)
 	return tt1 + t + t;
 }
 
-// width of integrated bandlimited step relative to the sample period
-const float INTEGRATED_POLYBLEP_WIDTH = 1.5f;
+// IntegratedPolyBLEP
+// Polynomial correction for C1 (slope) discontinuity
 
-// symbolically-integrated PolyBLEP
-inline float IntegratedPolyBLEP(float t, float w, float s)
+// width of integrated bandlimited step relative to the sample period
+const float INTEGRATED_POLYBLEP_WIDTH = 1.0f;
+
+// approximation of s * (w * (1 - 2 / pi * cos(t * pi / (2 * w)) - fabsf(t))
+
+// general case for a slope step of s units
+inline float IntegratedPolyBLEP(float const t, float const w, float const s)
 {
-	if (fabsf(t) >= w)
+	float at = fabsf(t);
+	if (at >= w)
 		return 0.0f;
-	t /= w;
-	float const t2 = t * t;
-	float const t3 = t2 * t;
-	float t3t = t3 * 0.33333333f + t;
-	if (t >= 0)
-		t3t = -t3t;
-	return (t3t + t2 + 0.33333333f) * 0.5f * s * w;
+	at /= w;
+	float const t2 = at * at;
+#if 1
+	float const t4 = t2 * t2;
+	return (0.375f - at + 0.75f * t2 - 0.125f * t4) * w * s * 0.5f;
+#else
+	float const t3 = t2 * at;
+	return (0.33333333f - at + t2 - 0.33333333f * t3) * w * s * 0.5f;
+#endif
 }
 
 // special case for a slope step of 8 units (triangle -4 to +4)
-#if 0
-#define IntegratedPolyBLEP(t, w) IntegratedPolyBLEP(t, w, 8)
-#else
-inline float IntegratedPolyBLEP(float t, float w)
+inline float IntegratedPolyBLEP(float const t, float const w)
 {
-	if (fabsf(t) >= w)
+	float at = fabsf(t);
+	if (at >= w)
 		return 0.0f;
-	t /= w;
-	float const t2 = t * t;
-	float const t3 = t2 * t;
-	float t3t = t3 * 0.33333333f + t;
-	if (t >= 0)
-		t3t = -t3t;
-	return (t3t + t2 + 0.33333333f) * 4 * w;
-}
+	at /= w;
+	float const t2 = at * at;
+#if 1
+	float const t4 = t2 * t2;
+	return (0.375f - at + 0.75f * t2 - 0.125f * t4) * w * 4;
+#else
+	float const t3 = t2 * at;
+	return (0.33333333f - at + t2 - 0.33333333f * t3) * w * 4;
 #endif
+}
