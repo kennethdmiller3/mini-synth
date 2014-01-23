5d46f261ed5c7101e1eb3a5f00777f25469c7352
kennethdmiller3@gmail.com <kennethdmiller3@gmail.com@23c9ecb3-a1c3-0e6f-be60-cf8a5c0acfb7>
2014-01-23 10:08:49 +0000

    * Amplifier config defaults to using key velocity

--- Amplifier.cpp
+++ Amplifier.cpp
@@ -10,7 +10,7 @@ Amplifier
 #include "Voice.h"
 
 // amplifier configuration
-AmplifierConfig amp_config(1.0f, 0.0f);
+AmplifierConfig amp_config(0.0f, 1.0f);
 
 // amplifier envelope configuration
 EnvelopeConfig amp_env_config(false, 0.0f, 1.0f, 1.0f, 0.1f);
