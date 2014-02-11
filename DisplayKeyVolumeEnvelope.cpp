71aeb170965df9f32cd16525c8db6ba40fbe836a
kennethdmiller3@gmail.com <kennethdmiller3@gmail.com@23c9ecb3-a1c3-0e6f-be60-cf8a5c0acfb7>
2014-02-11 08:14:06 +0000

    * Clean up key volume display

--- DisplayKeyVolumeEnvelope.cpp
+++ DisplayKeyVolumeEnvelope.cpp
@@ -43,21 +43,21 @@ void InitKeyVolumeEnvelopeDisplay(HANDLE hOut)
 
 void UpdateKeyVolumeEnvelopeDisplay(HANDLE hOut)
 {
-	WORD note_env_attrib[NOTES];
-	memset(note_env_attrib, env_attrib[0], sizeof(note_env_attrib));
+	WORD note_env_attrib[SPECTRUM_WIDTH];
 	WORD voice_env_attrib[VOICES];
-	memset(voice_env_attrib, env_attrib[0], sizeof(voice_env_attrib));
+
+	memset(note_env_attrib, env_attrib[EnvelopeState::OFF], sizeof(note_env_attrib));
 	for (int v = 0; v < VOICES; ++v)
 	{
-		EnvelopeState::State state = amp_env_state[v].state;
-		if (state != EnvelopeState::OFF)
-			note_env_attrib[voice_note[v]] = voice_env_attrib[v] = env_attrib[state];
+		EnvelopeState::State const state = amp_env_state[v].state;
+		WORD const attrib = env_attrib[state];
+		int const x = key_pos.X - keyboard_octave * 12 + voice_note[v];
+		if (x >= 0 && x < SPECTRUM_WIDTH)
+			note_env_attrib[x] = attrib;
+		voice_env_attrib[v] = attrib;
 	}
 
 	DWORD written;
-	int x = key_pos.X - keyboard_octave * 12;
-	int x0 = Max(x, 0);
-	int x1 = Min(x + NOTES, 80);
-	WriteConsoleOutputAttribute(hOut, note_env_attrib - x, x1 - x0, { 0, key_pos.Y }, &written);
+	WriteConsoleOutputAttribute(hOut, note_env_attrib, SPECTRUM_WIDTH, { 0, key_pos.Y }, &written);
 	WriteConsoleOutputAttribute(hOut, voice_env_attrib, VOICES, voice_pos, &written);
 }
