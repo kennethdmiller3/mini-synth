21c2642b1203834b09c40aaeb4aaf5572299dd71
kennethdmiller3@gmail.com <kennethdmiller3@gmail.com@23c9ecb3-a1c3-0e6f-be60-cf8a5c0acfb7>
2014-02-16 06:13:06 +0000

    * Show cutoff frequency in the filter menu title bar - Useful when tuning the cutoff to a specific frequency

---
+++ trunk/DisplayFilterFrequency.h
@@ -0,0 +1,9 @@
+#pragma once
+/*
+MINI VIRTUAL ANALOG SYNTHESIZER
+Copyright 2014 Kenneth D. Miller III
+
+Filter Frequency Display
+*/
+
+extern void UpdateFilterFrequencyDisplay(HANDLE hOut, int const v);
